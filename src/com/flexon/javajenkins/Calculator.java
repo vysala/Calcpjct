4fd087640fab2eb8fde3d7a0dc09689f83ec180c
vysala <vysala.anu@gmail.com>
2020-01-24 20:19:55 -0800

    added 2 files

---
+++ src/com/flexon/javajenkins/Calculator.java
@@ -0,0 +1,74 @@
+package com.flexon.javajenkins;
+import java.util.*;
+
+public class Calculator {
+	
+	private static int firstnumber=8;
+	private static int secondnumber=4;
+	
+	public static Integer sum(Integer x, Integer y)
+	{
+	 if (x == null || y == null)
+	 return null;
+	// System.out.println(x+y);
+	 return x + y;
+		}
+	
+	public static Integer difference(Integer x, Integer y)
+	{
+	 if (x == null || y == null)
+	 return null;
+	 return x - y;
+	}
+	
+	public static Integer multiply(Integer x, Integer y)
+	{
+		 if (x == null || y == null)
+		 return null;
+		 return x * y;
+		}
+
+	public static Integer division(Integer x, Integer y)
+	{
+		 if (x == null || y == null)
+		 return null;
+		 return x / y;
+		} 
+
+	public static void calculator() {
+	    System.out.println("Basic calculator");
+	    System.out.println("1. Addition");
+	    System.out.println("2. Subtraction");
+	    System.out.println("3. Multiplication");
+	    System.out.println("4. Division");
+	    System.out.println("Choose one:");
+	
+}
+	
+	public static void main(String[] args) {
+		 Scanner input = new Scanner(System.in);
+		 calculator();
+		 int choice = input.nextInt();
+		 input.close();
+		 switch(choice){
+		 case 1:
+			   System.out.println(sum(firstnumber,secondnumber));
+			    break;
+		 case 2:
+			   System.out.println(difference(firstnumber,secondnumber));
+			    break;
+		 case 3: 
+			 System.out.println(multiply(firstnumber,secondnumber));
+			    break;
+		 case 4:
+			 System.out.println(division(firstnumber,secondnumber));
+			    break;
+		 default:
+			    System.out.println("Wrong Choice");
+		    	break;
+		 }
+	 }
+		 
+	}
+	    
+	
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,59 @@
+package test.java;
+import com.flexon.javajenkins.Calculator;
+import static org.junit.Assert.*;
+
+
+// since class methods and variables are static I can use that here without instantiating
+
+public class CalculatorTest {
+	
+	public void testSum() {
+	    System.out.println("Sum");
+	    Integer x = 15;
+	    Integer y = 15;
+	    Integer expResult = 30;
+	    Integer result = Calculator.sum(x, y);
+	    assertEquals(expResult, result);
+	    assertEquals(null, Calculator.sum(15, null));
+	    assertEquals(null, Calculator.sum(null, 15));
+	    assertEquals(null, Calculator.sum(null, null));
+	  }
+	
+	public void testdiff() {
+	    System.out.println("Difference");
+	    Integer x = 15;
+	    Integer y = 15;
+	    Integer expResult = 0;
+	    Integer result = Calculator.difference(x, y);
+	    assertEquals(expResult, result);
+	    assertEquals(null, Calculator.difference(15, null));
+	    assertEquals(null, Calculator.difference(null, 15));
+	    assertEquals(null, Calculator.difference(null, null));
+	  }
+	
+	public void testmultiply() {
+	    System.out.println("Multiply");
+	    Integer x = 5;
+	    Integer y = 5;
+	    Integer expResult = 25;
+	    Integer result = Calculator.multiply(x, y);
+	    assertEquals(expResult, result);
+	    assertEquals(null, Calculator.multiply(5, null));
+	    assertEquals(null, Calculator.multiply(null, 5));
+	    assertEquals(null, Calculator.multiply(null, null));
+	  }
+	
+	public void testdivision() {
+	    System.out.println("Division");
+	    Integer x = 15;
+	    Integer y = 1;
+	    Integer expResult = 15;
+	    Integer result = Calculator.division(x, y);
+	    assertEquals(expResult, result);
+	    assertEquals(null, Calculator.division(15, null));
+	    assertEquals(null, Calculator.division(null, 15));
+	    assertEquals(null, Calculator.division(null, null));
+	  }
+	
+	
+	}
